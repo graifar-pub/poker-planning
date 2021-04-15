87896d355028f3ac582b7a54f1f308df87fe6f9d
Michele Fabris <michele.fabris@athonet.com>
2021-04-15 13:59:08 +0200

    Added server file with a different example, using MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-import express from 'express';
+var express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hi There');
+app.use(express.static(__dirname));
+
+app.get('/hi', function (req, res) {
+  res.send('Hi There you');
 });
 
 const port = 3000;
 
-app.listen(port, function () {
-  console.log('Listening on port 3000!')
+var server = app.listen(port, function () {
+  console.log('Listening on port', server.address().port);
 })
 
